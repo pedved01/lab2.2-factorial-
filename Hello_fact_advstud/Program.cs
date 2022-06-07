6347bd9121e87e9c91a9940de8089a68621c58ac
pedved <somichandriicz@gmail.com>
2022-06-07 18:48:51 +0200

    Добавьте файлы проекта.

---
+++ Hello_fact_advstud/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_fact_advstud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your value");
+           int value = int.Parse(Console.ReadLine());
+           Console.WriteLine($"result : {fact(value)}");
+            Console.ReadLine();
+        }
+
+        static int fact(int value)
+  
+        {
+            if (value == 1) 
+                return 1;
+            if (value == 0)
+                return 1;
+            if (value < 0)
+                return -1;
+            return value * fact(value - 1);
+        }
+    }
+
+    
+
+}
